098db31abf281d176d725c52f89ce8380642134f
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-02-13 15:08:11 -0300

    adding graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import { UsersModule } from './routes/users/users.module';
 
-
-
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      playground: true,
-      autoSchemaFile: 'src/schema.gql'
-    }),
-    UsersModule
-  ],
+	imports: [
+		GraphQLModule.forRoot({
+			playground: true,
+			autoSchemaFile: 'src/schema.gql',
+			formatError: (error: GraphQLError): GraphQLFormattedError => {
+				const graphQLFormattedError: GraphQLFormattedError = {
+					message: error.extensions.exception.response.message || error.extensions.code,
+					extensions: {
+						code: error.extensions.code,
+						statusCode: error.extensions.exception.response.statusCode
+					}
+				};
+
+				return graphQLFormattedError;
+			},
+		}),
+		UsersModule
+	],
 })
 
 export class AppModule {}
