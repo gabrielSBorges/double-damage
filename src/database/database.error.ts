c66fdfbbe9905cb83bb044424f5316c2815fcbf1
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-02-14 23:44:17 -0300

    creating database error formatter

---
+++ src/database/database.error.ts
@@ -0,0 +1,36 @@
+import { BadRequestException } from "@nestjs/common";
+import { polyglot } from "polyglot-rpg";
+
+export function databaseError(error: DataBaseError): BadRequestException {
+    
+    switch(error.errno) {
+        case 1062:
+            const key = getKeyFromErrorMessage(error.sqlMessage, error.parameters);    
+
+            return new BadRequestException(polyglot.database.locale('duplicate_key', 'ptBR', { key }));
+
+        default:
+            return new BadRequestException(polyglot.misc.locale('internal_server_error', 'ptBR'));
+    }
+}
+
+class DataBaseError {
+    code: string;
+    errno: number;
+    sqlState: string;
+    sqlMessage: string;
+    query: string;
+    parameters: any[];
+}
+
+function getKeyFromErrorMessage(message: string, parameters: any[]) {
+    let key = '<missing_key>';
+    
+    for (const item of parameters) {
+        if (message.includes(item)) {
+            key = item;
+        }
+    }
+
+    return key;
+}
